58a8a24fcafd2142b3155c8768b1132768ae3ce8
unknown <jmsrsd@gmail.com>
2017-12-16 21:39:17 +0700

    prepare for modularize app

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jmsrsd\PycharmProjects\JBLImageClassifier\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
