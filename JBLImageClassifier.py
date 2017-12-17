0ab6c10f8253f402dd0ae0dcfa5adbe1ddd2a0dc
unknown <jmsrsd@gmail.com>
2017-12-17 21:51:21 +0700

    finish version 1.0

--- JBLImageClassifier.py
+++ JBLImageClassifier.py
@@ -1,7 +1,12 @@
 import os
 
 import imutils
+import math
+
+import numpy as np
 from cv2 import cv2
+
+from matplotlib import pyplot as plt
 
 import Helper
 
@@ -49,153 +54,112 @@ def getDataset():
     return datasetFeatureVectors, datasetHists
 
 
-def classifyImageByHist(imagePath, datasetHists):
+def classifyImageByHist(imagePath, datasetHists, showStep=False):
+    imageName = os.path.basename(imagePath)
+
     imageBGR = cv2.imread(imagePath)
 
     imageFeatureVector = imageBGR2FeatureVector(imageBGR)
-    # imageHist = imageBGR2FlatHist(imageBGR)
+    imageHist = imageBGR2FlatHist(imageBGR)
 
+    result = []
+    resultLastIndex = 0
+
+    if showStep:
+        for key, item in datasetHists.items():
+            for i in range(len(datasetHists[key])):
+                imageHistFormatted = []
+                print("\n" + key + "[" + str(i) + "]: ")
+                for j in range(len(datasetHists[key][i])):
+                    imageHistFormatted.append('{0:.5f}'.format(datasetHists[key][i][j]))
+
+                for n in range(len(imageHistFormatted)):
+                    # if n % 8 == 0:
+                    # print('')
+
+                    print(imageHistFormatted[n], end=' ')
+
+                    if n == len(imageHistFormatted) - 1:
+                        print()
+
+        print("\nuji:")
+        for n in range(len(imageHist)):
+            print('{0:.5f}'.format(imageHist[n]), end=' ')
+
+            if n == len(imageHist) - 1:
+                print()
+
+        for key, item in datasetHists.items():
+            for i in range(len(datasetHists[key])):
+                imageHistFormatted = []
+                print("\nkomparasi uji dengan " + key + "[" + str(i) + "]: ")
+                for j in range(len(datasetHists[key][i])):
+                    buffer = min(imageHist[j], datasetHists[key][i][j])
+
+                    imageHistFormatted.append('{0:.5f}'.format(buffer))
+
+                for n in range(len(imageHistFormatted)):
+                    print(imageHistFormatted[n], end=' ')
+
+                    if n == len(imageHistFormatted) - 1:
+                        print()
+
+                imageHistFormatted = np.array(imageHistFormatted).astype(np.float);
+                result.append((key, sum(imageHistFormatted)))
+
+                # print("hasil sum: %f" % (result[resultLastIndex][1]))
+                resultLastIndex += 1
+    else:
+        for key, value in datasetHists.items():
+            for i in range(6):
+                imageHistDataset = value[i]
+
+                histValue = '{0:.5f}'.format(cv2.compareHist(imageHist, imageHistDataset, cv2.HISTCMP_INTERSECT))
+                result.append((key, float(histValue)))
+
+                # print(str(key) + "[" + str(i) + "]: " + str(result[resultLastIndex][1]))
+                resultLastIndex += 1
+
+    print("[ K O M P A R A S I   G A M B A R   %s   D E N G A N   D A T A S E T ]" % (imageName))
+    result.sort(key=lambda tup: tup[1], reverse=True)
+    for r in result:
+        print(r)
+
+    classes = []
     for key, value in datasetHists.items():
-        for i in range(6):
-            imageHist = value[i]
+        classes.append((key, 0))
 
-            result = cv2.compareHist(imageHist, datasetHists['sehat'][0], cv2.HISTCMP_KL_DIV)
+    k = 5
+    print()
+    print("[ P E M B E R I A N   R A N K I N G   D E N G A N   K   =   %d ]" % (k))
+    for i in range(len(classes)):
+        for j in range(k):
+            if result[j][0] == classes[i][0]:
+                classes[i] = (classes[i][0], classes[i][1] + 1)
 
-            print(str(key)  + "[" + str(i) + "]: " + str(result))
+    classes.sort(key=lambda tup: tup[1], reverse=True)
+    for c in classes:
+        print(c)
+
+    print()
+    print("[ H A S I L   K L A S I F I K A S I ]")
+    print("Kelas gambar %s adalah %s" % (imageName, classes[0][0]))
+    print("________________________________________________________________________________\n")
 
 
 def main():
     datasetFeatureVectors, datasetHists = getDataset()
     print(Helper.getMainDirectoryPath())
 
-    classifyImageByHist('C:/Users/jmsrsd/PycharmProjects/JBLImageClassifier/dataset/sehat37-5.jpg', datasetHists)
+    datatest = ['C:/Users/jmsrsd/PycharmProjects/JBLImageClassifier/dataset/sehat37-5.jpg',
+                'C:/Users/jmsrsd/PycharmProjects/JBLImageClassifier/dataset/sehat38-5.jpg',
+                'C:/Users/jmsrsd/PycharmProjects/JBLImageClassifier/dataset/sehat39-5.jpg',
+                'C:/Users/jmsrsd/PycharmProjects/JBLImageClassifier/dataset/sehat40-5.jpg']
+
+    for i in range(len(datatest)):
+        classifyImageByHist(datatest[i], datasetHists,
+                            showStep=False)
 
 
 if __name__ == '__main__':
     main()
-
-'''
-def hsv_color(img):
-    if isinstance(img, Image.Image):
-        r, g, b = img.split()
-        h_result = []
-        s_result = []
-        v_result = []
-        for rd, gn, bl in zip(r.getdata(), g.getdata(), b.getdata()):
-            h, s, v = colorsys.rgb_to_hsv(rd / 255., gn / 255., bl / 255.)
-            h_result.append(int(h * 255.))
-            s_result.append(int(s * 255.))
-            v_result.append(int(v * 255.))
-        r.putdata(h_result)
-        g.putdata(s_result)
-        b.putdata(v_result)
-        return Image.merge('RGB', (r, g, b))
-    else:
-        return None
-
-
-def normalize_hist(hist, pixel_size):
-    for v in hist:
-        v = int(v / pixel_size)
-
-    return hist
-
-
-def load_image_from_dir_to_hist(path):
-    img = Image.open(path)
-    img = hsv_color(img)
-    img = numpy.array(img)
-
-    pixel_size = img.size
-
-    hist_h = normalize_hist(cv2.calcHist([img], [0], None, [256], [0, 256]), pixel_size)
-    hist_s = normalize_hist(cv2.calcHist([img], [1], None, [256], [0, 256]), pixel_size)
-    hist_v = normalize_hist(cv2.calcHist([img], [2], None, [256], [0, 256]), pixel_size)
-
-    print("%s %s %s" % (hist_h, hist_s, hist_v))
-
-    return hist_h, hist_s, hist_v
-
-
-def load_images_from_dir_to_hist(path):
-    hist_list = []
-    for filename in glob.glob(path + '/*.jpg'):
-        img = Image.open(filename)
-        img = hsv_color(img)
-        img = numpy.array(img)
-
-        pixel_size = img.size
-
-        hist_h = normalize_hist(cv2.calcHist([img], [0], None, [256], [0, 256]), pixel_size)
-        hist_s = normalize_hist(cv2.calcHist([img], [1], None, [256], [0, 256]), pixel_size)
-        hist_v = normalize_hist(cv2.calcHist([img], [2], None, [256], [0, 256]), pixel_size)
-        hist_list.append((hist_h, hist_s, hist_v))
-
-    return hist_list
-
-
-base_path = 'C:/Users/jmsrsd/PycharmProjects/JBLImageClassifier/Daun jeruk'
-
-cendawanjelaga_list = load_images_from_dir_to_hist(base_path + '/cendawanjelaga')
-cvpd_list = load_images_from_dir_to_hist(base_path + '/cvpd')
-kekuranganzn_list = load_images_from_dir_to_hist(base_path + '/kekuranganzn')
-mildew_list = load_images_from_dir_to_hist(base_path + '/mildew')
-sehat_list = load_images_from_dir_to_hist(base_path + '/sehat')
-
-class_label_list = ['cendawan jelaga', 'cvpd', 'kekurangan zn', 'mildew', 'sehat']
-
-dataset = [cendawanjelaga_list, cvpd_list, kekuranganzn_list, mildew_list, sehat_list];
-
-
-def knn(test):
-    column = len(dataset[0])
-    row = len(dataset)
-
-    dataset_linear = []
-    distance_list = []
-
-    for i, r in enumerate(dataset):
-        for j, c in enumerate(dataset[i]):
-            dataset_linear.append(dataset[i][j])
-
-    for i, item in enumerate(dataset_linear):
-        distance = abs(test[0] - dataset_linear[i][0]) + abs(test[1] - dataset_linear[i][1]) + abs(
-            test[2] - dataset_linear[i][2])
-        distance_list.append(distance)
-
-    print(distance_list)
-
-    k = 3
-    nearest_index = [-1, -1, -1]
-
-    while k > 0:
-        min_index = 0
-        min_value = 256
-        for i, item in enumerate(distance_list):
-            if min_value >= item and i != nearest_index[0] and i != nearest_index[1] and i != nearest_index[2]:
-                min_index = i
-                min_value = item
-
-        k -= 1
-        nearest_index[k] = min_index
-
-    class_score = [0, 0, 0]
-
-    for i in range(len(class_score)):
-        for j in range(len(class_score)):
-            if int(nearest_index[i] / column) == int(nearest_index[j] / column):
-                class_score[i] += 1
-
-    max_index = 0
-    max_value = 0
-    for i in range(len(class_score)):
-        if max_value <= class_score[i]:
-            max_index = i
-            max_value = class_score[i]
-
-    return class_label_list[int(nearest_index[max_index] / column)]
-
-
-if len(sys.argv) > 1:
-    knn(load_image_from_dir_to_hist(sys.argv[1]))
-'''
